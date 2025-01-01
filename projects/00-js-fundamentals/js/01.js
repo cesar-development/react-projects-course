87e6de40bd4f3524deb9dd8b52e0aa11d0aaad35
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-12-31 21:51:37 -0600

    feat(Root): Add 01 and 02 fundamentals

---
+++ projects/00-js-fundamentals/js/01.js
@@ -0,0 +1,16 @@
+// Variables - let
+let client = 'Juan'
+const discountPrice = 220 // camelCase
+
+// Re asignar
+client = 20
+client = true
+
+// Pueden iniciar sin un valor
+let price
+price = 1000
+
+console.log(price)
+
+console.log(client)
+console.log(discountPrice)
---
+++ projects/00-js-fundamentals/js/02.js
@@ -0,0 +1,13 @@
+// Variables - const
+const client = 'Juan'
+const discountPrice = 220
+
+// No se puede re asignar
+// cliente = 'Pablo'
+
+// No pueden iniciar sin valor
+// const precio
+
+// console.log(precio)
+console.log(client)
+console.log(discountPrice)
