58d05a9624ff409a0a0d9531c877b4f4304037bb
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-01-01 22:18:21 -0600

    feat(Root): Add 13 and 14 fundamentals

---
+++ projects/00-js-fundamentals/js/13.js
@@ -0,0 +1,8 @@
+// Funciones - Function Expression
+
+const add = function (number1 = 0, number2 = 0) {
+  return number1 + number2
+}
+
+const result = add(30)
+console.log(result)
---
+++ projects/00-js-fundamentals/js/14.js
@@ -0,0 +1,8 @@
+// Funciones - Arrow Functions
+// const sumar = (numero, numero2) => numero + 20
+
+const addArrow = () => 2 + 2
+
+const result = addArrow()
+
+console.log(result)
